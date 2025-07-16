a260f52e9170eb6adbafc565aef0c3de63a30ac3
celeste456 <85599011+celeste456@users.noreply.github.com>
2025-07-16 14:45:39 -0600

    allure util fix

--- src/main/java/utils/AllureUtils.java
+++ src/main/java/utils/AllureUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
 import model.CheckoutData;
 import model.UserData;
 
@@ -8,23 +9,24 @@ import java.util.Map;
 
 public class AllureUtils {
 
-    public static void logUserData(UserData user) {
-        String data = "Gender: " + user.getGender() + "\n"
+    @Attachment(value = "User Data", type = "text/plain")
+    public static String logUserData(UserData user) {
+        return "Gender: " + user.getGender() + "\n"
                 + "First Name: " + user.getFirstName() + "\n"
                 + "Last Name: " + user.getLastName() + "\n"
                 + "Email: " + user.getEmail() + "\n"
                 + "Password: " + user.getPassword();
-        Allure.addAttachment("User Data", data);
     }
 
-    public static void logLoginData(UserData user) {
-        String data = "Email: " + user.getEmail() + "\n"
+    @Attachment(value = "Login Credentials", type = "text/plain")
+    public static String logLoginData(UserData user) {
+        return "Email: " + user.getEmail() + "\n"
                 + "Password: " + user.getPassword();
-        Allure.addAttachment("Login Credentials", data);
     }
 
-    public static void logCheckoutData(CheckoutData data) {
-        String content = "First Name: " + data.getFirstName() + "\n"
+    @Attachment(value = "Checkout Data", type = "text/plain")
+    public static String logCheckoutData(CheckoutData data) {
+        return "First Name: " + data.getFirstName() + "\n"
                 + "Last Name: " + data.getLastName() + "\n"
                 + "Email: " + data.getEmail() + "\n"
                 + "Company: " + data.getCompany() + "\n"
@@ -34,10 +36,10 @@ public class AllureUtils {
                 + "ZIP: " + data.getZip() + "\n"
                 + "Phone: " + data.getPhone() + "\n"
                 + "Fax: " + data.getFax();
-        Allure.addAttachment("Checkout Data", content);
     }
 
-    public static void logAssertionMessage(String message) {
-        Allure.addAttachment("Assertion Detail", message);
+    @Attachment(value = "Assertion Detail", type = "text/plain")
+    public static String logAssertionMessage(String message) {
+        return message;
     }
 }
